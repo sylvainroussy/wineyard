f2a72a9fc76b2833f79c410c832d334d06c6f3f6
Sylvain ROUSSY <sylvain.roussy@free.fr>
2019-05-07 10:42:12 +0200

    First commit

---
+++ src/main/java/fr/srosoft/wineyard/core/main/WineyardApplication.java
@@ -0,0 +1,26 @@
+package fr.srosoft.wineyard.core.main;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.Servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan (basePackages={"fr.srosoft"})
+public class WineyardApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+        SpringApplication.run(WineyardApplication.class, args);
+    }
+	
+	@Bean
+    public ServletRegistrationBean<Servlet> servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        return new ServletRegistrationBean<Servlet>(servlet, "*.jsf");
+    }
+}
